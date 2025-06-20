379c6362c315c0cbecb6efe0a8f261017ab8b79a
Rafael Ribeiro <rafael@ascitech.com.br>
2025-06-20 19:39:09 -0300

    Update __init__.py

--- traknor/infrastructure/accounts/__init__.py
+++ traknor/infrastructure/accounts/__init__.py
@@ -2,4 +2,3 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()  # Isso evita carregar o model antes da hora
 
-default_app_config = "traknor.infrastructure.accounts.apps.AccountsInfraConfig"
